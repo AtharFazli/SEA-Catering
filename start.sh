b748ded8bbd5eac5d37fb147555929db35707882
AtharFazli <atharfs9@gmail.com>
2025-06-26 10:24:04 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-php artisan migrate --force
+
+# Cache konfigurasi
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-php artisan serve --host=0.0.0.0 --port=8080
+
+# Jalankan migrasi (jika perlu)
+php artisan migrate --force
+
+# Jalankan server di port yang diberikan Railway
+php artisan serve --host=0.0.0.0 --port=${PORT}
