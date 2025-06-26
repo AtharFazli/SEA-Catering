ce44236f038c2e199e3272f796ef6064d13b88ee
AtharFazli <atharfs9@gmail.com>
2025-06-26 11:07:51 +0700

    fix start script

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env bash
 
-# Cache konfigurasi
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-
-# Jalankan migrasi (jika perlu)
 php artisan migrate --force
 
-# Jalankan server di port yang diberikan Railway
 php artisan serve --host=0.0.0.0 --port=${PORT}
